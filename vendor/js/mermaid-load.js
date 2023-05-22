8555fe9b09ae70326127343d8003ce3c87ec75fd
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-05-23 00:34:02 +0200

    fix(index): use correct math parser

--- vendor/js/mermaid-load.js
+++ vendor/js/mermaid-load.js
@@ -19,6 +19,43 @@ function uniqueIds(svgString, idx) {
     return svgString.replace(re, `#$1-${idx}`);
 }
 
+function mermaidParser() {
+    const graphs = document.getElementsByClassName("mermaid");
+    graphs.forEach(async (item, index) => {
+        const graphCode = item.textContent.trim();
+        const mermaidDiv = document.createElement("div");
+        mermaidDiv.classList.add("mermaid");
+        mermaidDiv.setAttribute("data-processed", "true");
+
+        try {
+            const { svg } = await mermaid.render(`graph-${index}`, graphCode);
+            mermaidDiv.innerHTML = uniqueIds(svg, index);
+            item.parentNode.parentNode.insertBefore(
+                mermaidDiv,
+                item.parentNode
+            );
+            item.parentNode.remove();
+        } catch (err) {
+            console.error(`Cannot render diagram ${index}\n${graphCode}`);
+            console.error(err.message);
+        }
+    });
+}
+
+function mathParser() {
+    const maths = document.evaluate('//p[starts-with(., "$$")]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0; i < maths.snapshotLength; i++) {
+        // Substitute any <em> tags with _ to avoid conflicts with markdown
+        maths.snapshotItem(i).innerHTML = maths.snapshotItem(i).innerHTML.replace(/<em>/g, "_").replace(/<\/em>/g, "_");
+        console.log(maths.snapshotItem(i).innerHTML);
+        
+        const codeEl = document.createElement("code");
+        codeEl.innerHTML = maths.snapshotItem(i).textContent;
+        maths.snapshotItem(i).innerHTML = "";
+        maths.snapshotItem(i).appendChild(codeEl);
+    }
+}
+
 function loadMermaid() {
     mermaid.initialize({
         startOnLoad: false,
@@ -26,26 +63,8 @@ function loadMermaid() {
         flowchart: { htmlLabels: false },
     });
     Reveal.addEventListener("ready", (event) => {
-        const graphs = document.getElementsByClassName("mermaid");
-        graphs.forEach(async (item, index) => {
-            const graphCode = item.textContent.trim();
-            const mermaidDiv = document.createElement("div");
-            mermaidDiv.classList.add("mermaid");
-            mermaidDiv.setAttribute("data-processed", "true");
-
-            try {
-                const { svg } = await mermaid.render(`graph-${index}`, graphCode);
-                mermaidDiv.innerHTML = uniqueIds(svg, index);
-                item.parentNode.parentNode.insertBefore(
-                    mermaidDiv,
-                    item.parentNode
-                );
-                item.parentNode.remove();
-            } catch (err) {
-                console.error(`Cannot render diagram ${index}\n${graphCode}`);
-                console.error(err.message);
-            }
-        });
+        mermaidParser();
+        mathParser();
     });
 }
 
